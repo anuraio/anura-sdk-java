c6cf6ba88571df7cf42d47c070fc7e0e429bf3bc
Xavier Mejia <xmejia@anura.io>
2024-10-16 11:17:27 -0500

    add tests

--- src/test/java/io/anura/sdk/AnuraDirectTest.java
+++ src/test/java/io/anura/sdk/AnuraDirectTest.java
@@ -1,4 +1,44 @@
+package io.anura.sdk;
+
+import io.anura.sdk.exceptions.AnuraClientException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AnuraDirectTest {
-  
+    @Test
+    @DisplayName("Empty Instance Should Throw Anura Client Exception")
+    void emptyInstance() {
+        DirectRequestBuilder builder = new DirectRequestBuilder();
+        DirectRequest request = builder.setIpAddress("127.0.0.1").build();
+
+        AnuraDirect direct = new AnuraDirect(true);
+        assertThrows(AnuraClientException.class, () -> direct.getResult(request));
+    }
+
+    @Test
+    @DisplayName("Invalid Instance Should Throw Anura Client Exception")
+    void invalidInstance() {
+        DirectRequestBuilder builder = new DirectRequestBuilder();
+        DirectRequest request = builder.setInstanceId("abcdefg").setIpAddress("127.0.0.1").build();
+
+        AnuraDirect direct = new AnuraDirect(true);
+        assertThrows(AnuraClientException.class, () -> direct.getResult(request));
+    }
+
+    @Test
+    @DisplayName("Can add additional data")
+    void addAdditionalData() {
+     var builder = new DirectRequestBuilder();
+     var request = builder.build();
+
+     var additionalDataMap = new HashMap<String, String>();
+     additionalDataMap.put("1", "test");
+     request = builder.setAdditionalData(additionalDataMap).build();
+
+     assertEquals(request.getAdditionalData().get("1"), "test");
+    }
 }
